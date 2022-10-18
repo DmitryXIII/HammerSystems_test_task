64b296517475fddc24876c559d6c54ef51641f43
Dmitry Gusev <dmtrxiii@gmail.com>
2022-10-18 12:44:22 +0300

    toolbar collapsing released

---
+++ app/src/main/java/com/avacodo/hammersystemstesttask/MenuFragment.kt
@@ -0,0 +1,11 @@
+package com.avacodo.hammersystemstesttask
+
+import android.os.Bundle
+import android.view.View
+import com.avacodo.hammersystemstesttask.databinding.FragmentMenuBinding
+
+class MenuFragment: BaseFragment<FragmentMenuBinding, String>(FragmentMenuBinding::inflate) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
